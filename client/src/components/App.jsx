28cb4a13bddfb4927d9495c4381345ff9acc3e7c
Kris Rawson <krisrawson@Rawsons-iMac.local>
2021-01-20 22:35:06 -0700

    Zoom function is 95% done.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,24 @@
+/* eslint-disable react/destructuring-assignment */
+import axios from 'axios';
 import React from 'react';
 import PhotoGal from './PhotoGal';
 import PhotoNav from './PhotoNav';
+import Zoom from './Zoom';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      items: [],
+    };
+    this.getItems = this.getItems.bind(this);
+  }
+
+  getItems() {
+    axios.get('/api/items')
+      .then(({ data }) => {
+        this.setState({ items: data });
+      });
   }
 
   render() {
@@ -14,8 +27,9 @@ class App extends React.Component {
         If you can see this, React is working.
         <div id="media-wrapper">
           <div className="inner-wrapper">
-            <PhotoGal />
-            <PhotoNav />
+            <PhotoGal items={this.state.items} />
+            {/* <Zoom /> */}
+            <PhotoNav items={this.state.items} />
           </div>
         </div>
       </div>
---
+++ client/src/components/Zoom.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Zoom = () => (
+  <div id="myresult" className="img-zoom-result" />
+);
+
+export default Zoom;
